00881d31632f443a525c46153227f1e4904b8ba6
samir-byte <kiranpoudel28@gmail.com>
2022-08-26 12:04:52 +0545

    types check for useref hook

--- src/refs/UserSearch.tsx
+++ src/refs/UserSearch.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const users = [
     {
@@ -16,8 +16,13 @@ const users = [
 ];
 
 const UserSearch: React.FC = () => {
+    const inputRef = useRef<HTMLInputElement | null>(null);
     const [name, setName] = useState<string>('');
     const [user, setUser] = useState<{name: string,age: number} | undefined>();
+
+    useEffect(() => {
+        inputRef.current?.focus()
+    },[]);
 
     const handleClick = () => {
         const foundUser = users.find((user)=> {
@@ -28,7 +33,7 @@ const UserSearch: React.FC = () => {
 
     return <div>
         User Search
-        <input value={name} onChange={e => setName(e.target.value)} />
+        <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
         <button onClick={handleClick}>Find</button>
         <div>
             {user?.name}
